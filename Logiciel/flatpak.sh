0051335d08ada4ceb35415831ef794393f909a31
Hoffmann Julien <29329710+luminahateo@users.noreply.github.com>
2024-06-09 01:19:27 +0200

    Correction

--- Logiciel/flatpak.sh
+++ Logiciel/flatpak.sh
@@ -9,7 +9,7 @@ flatpak install -y \
 org.libreoffice.LibreOffice 
 
 echo "Internet"
-flatpak install -y \ 
+flatpak install -y \
 org.mozilla.Thunderbird \
 com.tutanota.Tutanota \
 com.transmissionbt.Transmission \
@@ -21,7 +21,7 @@ echo "Graphisme"
 flatpak install -y \
 org.inkscape.Inkscape \
 org.kde.krita \
-org.gimp.GIMP \ 
+org.gimp.GIMP \
 org.gimp.GIMP.Manual \
 org.blender.Blender \
 org.kde.kdenlive \
@@ -31,24 +31,24 @@ org.darktable.Darktable \
 org.free_astro.siril
 
 echo "Programmation"
-flatpak install -y 
+flatpak install -y \
 dev.pulsar_edit.Pulsar \
 io.github.shiftey.Desktop \
 net.sonic_pi.SonicPi \
 org.godotengine.Godot3Sharp \
 org.godotengine.Godot3 \
-org.godotengine.GodotSharp \ 
+org.godotengine.GodotSharp \
 org.godotengine.Godot
 
 echo "Utilitaire"
-flatpak install -y 
+flatpak install -y \
 com.github.tchx84.Flatseal \
 org.gnome.FileRoller \
 app.drey.Dialect \
 org.cockpit_project.CockpitClient
 
 echo "Son et Video"
-flatpak install -y \ 
+flatpak install -y \
 com.github.taiko2k.tauonmb \
 com.spotify.Client \
 io.github.celluloid_player.Celluloid \
--- Logiciel/jeux.sh
+++ Logiciel/jeux.sh
@@ -4,22 +4,27 @@ clear
 echo "jeux
 ======================================================================================="
 
-flatpak install -y \ 
-net.lutris.Lutris \ 
+flatpak install -y \
+net.lutris.Lutris \
 com.valvesoftware.Steam \
-com.valvesoftware.Steam.CompatibilityTool.Proton
+com.valvesoftware.Steam.CompatibilityTool.Proton \
+net.davidotek.pupgui2 \
+com.github.Matoking.protontricks
 
-flatpak install -y \ 
-ca._0ldsk00l.Nestopia \ 
+flatpak install -y \
+ca._0ldsk00l.Nestopia \
 com.snes9x.Snes9x \
-io.github.sameboy.SameBoy \ 
-io.mgba.mGBA,org.desmume.DeSmuME \
+io.github.sameboy.SameBoy \
+io.mgba.mGBA \
+org.desmume.DeSmuME \
 org.DolphinEmu.dolphin-emu \
-io.github.shiiion.primehack \ 
+io.github.shiiion.primehack \
 io.github.lime3ds.Lime3DS \
-org.duckstation.DuckStation \ 
+org.duckstation.DuckStation \
 net.pcsx2.PCSX2 \
 org.ppsspp.PPSSPP \
-com.carpeludum.KegaFusion \ 
+com.carpeludum.KegaFusion \
 org.flycast.Flycast \
+app.xemu.xemu \
+net.rpcs3.RPCS3 \
 org.scummvm.ScummVM
